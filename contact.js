2da9febca909910f2d8fd9e9c942fc59fb51393e
Vaishnavi Uppula <51997891+VaishnaviUppula@users.noreply.github.com>
2022-10-31 22:27:40 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,22 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_1";
+    const templateID = "template_u80v7b8";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("your message has been sent! Thank you!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
